70692dd7ca608deebc5dfd04182513071480939a
vRyoutaSaitou <ryouta.saitou2@hd.veriserve.co.jp>
2026-02-24 17:46:08 +0900

    test_03
    Updated the header to always include 'remark' and added timestamp to the Excel filename.

--- 02_xlsx_conversion.py
+++ 02_xlsx_conversion.py
@@ -1,13 +1,27 @@
 import json
 import openpyxl
+from datetime import datetime 
+from zoneinfo import ZoneInfo
 
 # 入力・出力ファイル
+print("jsonファイル出力中")
+
 json_path = "report.json"
-excel_path = "テスト実行結果_20251128_150000.xlsx"
+
+print("jsonファイル出力完了")
+
+print("タイムスタンプ生成中")
+
+#タイムゾーンを設定し、タイムスタンプを導入
+ts = datetime.now(ZoneInfo("Asia/Tokyo")).strftime("%Y%m%d_%H%M%S")
+excel_path = f"テスト実行結果_{ts}.xlsx"
+
+print("タイムスタンプ生成完了")
 
 with open(json_path, "r", encoding="utf-8") as f:
   data = json.load(f)
 
+print("xslxファイル出力中")
 # Excel作成
 wb = openpyxl.Workbook()
 sheet = wb.worksheets[0]
@@ -15,16 +29,21 @@ sheet.title = "テスト結果"
 
 records = data["test_results"]
 
-header = ["test_case_name", "test_officer", "test_date", "result"]
-if any("remark" in r for r in records):
-  header.append("remark")
+header = ["test_case_name", "test_officer", "test_date", "result", "remark"]
 
+sheet.append(header)
 
+print("データ書き込み中")
 # データ行
 for record in data["test_results"]:
   row = [record.get(key, "") for key in header]
   sheet.append(row)
 
+print("データ書き込み完了")
+
 # 保存
 wb.save(excel_path)
 wb.close()
+
+print(f"ファイル名「{excel_path}」で保存しました")
+print("xlsxファイル出力完了")
